232a1280434102d44e378d247ded9f32b5173fce
Scott Gale <galejscott@gmail.com>
2019-05-07 08:45:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  
+end
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+  
+end
